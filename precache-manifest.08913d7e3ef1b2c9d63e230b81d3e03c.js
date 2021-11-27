b66df0e187ed3f3aa24363a8ffba4ac4b3c53c92
Aneta <anc@praqma.net>
2021-11-27 03:48:52 +0100

    Updates

---
+++ precache-manifest.08913d7e3ef1b2c9d63e230b81d3e03c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74657c4789fc11bb46a721355b15cad3",
+    "url": "/filmclub/index.html"
+  },
+  {
+    "revision": "da9d03502bf6f38292db",
+    "url": "/filmclub/static/css/2.05dd3ac6.chunk.css"
+  },
+  {
+    "revision": "a2c92e1c7ba32110666e",
+    "url": "/filmclub/static/css/main.d9d0fa46.chunk.css"
+  },
+  {
+    "revision": "da9d03502bf6f38292db",
+    "url": "/filmclub/static/js/2.daa4c299.chunk.js"
+  },
+  {
+    "revision": "dfc9d4e882c79073f5741fc9ff3191e1",
+    "url": "/filmclub/static/js/2.daa4c299.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2c92e1c7ba32110666e",
+    "url": "/filmclub/static/js/main.9d572db4.chunk.js"
+  },
+  {
+    "revision": "31691aa6761ac4472bb5",
+    "url": "/filmclub/static/js/runtime-main.a6665464.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/filmclub/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/filmclub/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/filmclub/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/filmclub/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/filmclub/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
